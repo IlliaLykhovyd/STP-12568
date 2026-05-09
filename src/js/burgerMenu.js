5b2ebb9f733670db8aa18d9c7668f3125b6596af
Illia <mddg354@gmail.com>
2026-05-10 00:37:57 +0300

    illia#2 sections

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,14 +1,36 @@
-const menu = document.querySelector('.mobile-menu-overlay');
-const burger = document.querySelector('#openMenuBtn');
-const burgerBtn = document.querySelector('.menu-btn');
-const closeBtn = document.querySelector('#closeMenuBtn');
+document.addEventListener('DOMContentLoaded', () => {
+  const openMenuBtn = document.getElementById('openMenuBtn');
+  const closeMenuBtn = document.getElementById('closeMenuBtn');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const menuLinks = document.querySelectorAll('.mobile-menu-links a');
 
-burgerBtn.addEventListener('click', () => {
-  menu.classList.add('active');
-  burger.classList.add('hidden');
-});
+  const openMenu = () => {
+    mobileMenu.classList.add('active');
+    document.body.classList.add('no-scroll');
+    document.documentElement.classList.add('no-scroll');
+  };
 
-closeBtn.addEventListener('click', () => {
-  menu.classList.remove('active');
-  burger.classList.remove('hidden');
+  const closeMenu = () => {
+    mobileMenu.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+    document.documentElement.classList.remove('no-scroll');
+  };
+
+  openMenuBtn.addEventListener('click', openMenu);
+  closeMenuBtn.addEventListener('click', closeMenu);
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
+
+  document.addEventListener('click', event => {
+    if (mobileMenu.classList.contains('active')) {
+      const isClickInsideMenu = mobileMenu.contains(event.target);
+      const isClickOnOpenBtn = openMenuBtn.contains(event.target);
+
+      if (!isClickInsideMenu && !isClickOnOpenBtn) {
+        closeMenu();
+      }
+    }
+  });
 });
